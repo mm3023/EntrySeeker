2b1121b4b8ee59db4af67230f7ed6b3109d699d5
mm3023 <mikemoore227@gmail.com>
2024-10-14 16:54:44 -0400

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,11 +3,18 @@ from django.http import HttpResponse
 
 def signal2():
     print("signal 2 that views is running");
-"""
+    
+"""    
 def my_view(request):
     return render(request, 'my_template.html', {'message': 'Hello from my view!'})
-"""    
-def index():
+"""
+
+
+def my_view(request):
+    return render(request, {'message': 'Hello from my view!'});
+    
+ 
+def index(request):
     print('---- Index triggered ------ ');
     return HttpResponse("Hello, world. You're at the polls index.");
 
