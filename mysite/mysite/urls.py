474e17d2d0553aa182117e67d71b7c2b4507ab4a
mm3023 <mikemoore227@gmail.com>
2024-10-14 12:50:12 -0400

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import polls.urls
+from polls.urls import *
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
