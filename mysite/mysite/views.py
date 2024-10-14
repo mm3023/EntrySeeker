d68ba2aac190a4f8ca1c6cdd5cf9d56a9dab5476
mm3023 <mikemoore227@gmail.com>
2024-10-14 16:10:50 -0400

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 """
 def my_view(request):
     return render(request, 'my_template.html', {'message': 'Hello from my view!'})
 """    
 def my_view(request):
     return render(request, 'my_template.html', {'message': 'Hello from my view!'})
+
+
+
+
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the not-polls index.")
