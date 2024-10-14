0457ec64d1617aa220471f85a6d736a303d35d48
mm3023 <mikemoore227@gmail.com>
2024-10-14 16:23:02 -0400

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from polls.urls import *
-from . import views
+#from . import views
 #import views
 
 
