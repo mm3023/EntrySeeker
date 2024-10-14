625132d38a02f4d6cfe9e77d8e486f6932f7be56
mm3023 <mikemoore227@gmail.com>
2024-10-14 15:15:13 -0400

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from polls.urls import *
+from . import views
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
+    path('my-view/', views.my_view, name='my-view')
 ]
+
+
+
