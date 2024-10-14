7f68d66a072d58d1093f7cbe7288fb0b3801c8ae
mm3023 <mikemoore227@gmail.com>
2024-10-14 16:24:47 -0400

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def signal2():
-    print("signal 2 that views is running");
-signal2();    
+    print("signal 2 that views is running"
 """
 def my_view(request):
     return render(request, 'my_template.html', {'message': 'Hello from my view!'})
