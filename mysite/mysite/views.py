63a3f739a5b4a719274067a515701214de7f6faa
mm3023 <mikemoore227@gmail.com>
2024-10-14 16:08:25 -0400

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-
+"""
+def my_view(request):
+    return render(request, 'my_template.html', {'message': 'Hello from my view!'})
+"""    
 def my_view(request):
     return render(request, 'my_template.html', {'message': 'Hello from my view!'})
