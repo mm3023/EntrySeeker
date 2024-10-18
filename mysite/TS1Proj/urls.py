8e09824a85cdce5cb4edd195f93bd5e982256dec
mm3023 <mikemoore227@gmail.com>
2024-10-18 14:40:27 -0400

    Update urls.py

--- mysite/TS1Proj/urls.py
+++ mysite/TS1Proj/urls.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
