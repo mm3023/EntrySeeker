edc2f4d8f868cc797905b7225fa0e4106b4b3518
mm3023 <mikemoore227@gmail.com>
2024-10-18 14:55:15 -0400

    Update views.py

--- mysite/TS1Proj/views.py
+++ mysite/TS1Proj/views.py
@@ -1,1 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
 
+
+    
+"""    
+def my_view(request):
+    return render(request, 'my_template.html', {'message': 'Hello from my view!'})
+
+
+
+def my_view(request):
+    return render(request, {'message': 'Hello from my view!'});
+    
+ 
+def index(request):
+    print('---- Index triggered ------ ');
+    return HttpResponse("Hello, world. You're at the polls index.");
+
+def entry(request):
+    #print('---- Main triggered ------ ');
+    return HttpResponse("You're at the main-entry.");
+"""
+    
+
+def index(request):
+    print('---- Index triggered ------ ');
+    return HttpResponse("Hello, world. You're at the TS1 index.");
