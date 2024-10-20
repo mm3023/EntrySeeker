c8c3c7eb1001360afc28019262fb94463a42c5da
mm3023 <mikemoore227@gmail.com>
2024-10-20 14:24:53 -0400

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = Path(/).resolve().parent.parent #this is an edit
+BASE_DIR = Path(/) #this is an edit
 
 
 # Quick-start development settings - unsuitable for production
