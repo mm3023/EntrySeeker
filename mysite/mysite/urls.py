dc50ef8a65ad2807b8d0563d9f4b2dba5ed09aa1
mm3023 <mikemoore227@gmail.com>
2024-10-22 08:32:26 -0400

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,41 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-"""
-from django.contrib import admin
-from django.urls import path, include
-from polls.urls import *
-from . import views
-#import views
 
-
-def sig():
-    print("Signal 1 that there is output");
-    
-sig();
-views.signal2();
-urlpatterns = [
-    #path("polls/", include("polls.urls")),
-    path('admin/', admin.site.urls),
-    #path('polls/', views.my_view, name='my-view'),
-    path('polls/', views.my_view),
-    path('polls/', views.index),
-]
-"""
 
 from django.contrib import admin
 from django.urls import path, include
 from polls.urls import *  # Import all URL patterns from polls.urls
-
 from . import views  # Import views from the current app
-def sig():
-    print("Signal 1 that there is output");
-    
-sig();
-views.signal2();
-
 import logging
-
 logger = logging.getLogger(__name__)
 
 def my_view(request):
@@ -60,7 +32,7 @@ def my_view(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('polls/', views.my_view),
-    path("polls/", include("polls.urls")),
+    #path("polls/", include("polls.urls")),
     path('', views.entry),
     path('ts/', include("TS1Proj.urls")),
     #path('polls/', views.index),  # Uncomment if you prefer the default view
