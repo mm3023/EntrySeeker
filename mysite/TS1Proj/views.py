8279fc17eee4bff03ae6f3ecff65953158e47e83
mm3023 <mikemoore227@gmail.com>
2024-10-22 11:00:16 -0400

    Update views.py

--- mysite/TS1Proj/views.py
+++ mysite/TS1Proj/views.py
@@ -5,7 +5,7 @@ def zeist_initiate():
     print("zeist is online");
     return 0;
 zeist_initiate();
-
+import requests;
 #response = requests.get('https://api.github.com/user', auth=('user', 'pass'))
 #https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25
 response = requests.get('https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25');
