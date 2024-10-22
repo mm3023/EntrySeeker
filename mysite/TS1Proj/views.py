eb25973c9ea4f4bd91fcaeb7145a98fa6d054681
mm3023 <mikemoore227@gmail.com>
2024-10-22 10:54:31 -0400

    Update views.py

--- mysite/TS1Proj/views.py
+++ mysite/TS1Proj/views.py
@@ -5,7 +5,18 @@ def zeist_initiate():
     print("zeist is online");
     return 0;
 zeist_initiate();
-https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25
+
+#response = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+#https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25
+response = requests.get('https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25');
+print('request sent');
+print('response');
+print(response);
+
+
+
+
+
 
 def index(request):
         context={'message': 'Hello from my TS1Proj!','title':'Verbal Octopus'}
