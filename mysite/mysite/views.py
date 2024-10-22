44ca6dde4a5b8f074f2e0a71e2da2abeeb941acb
mm3023 <mikemoore227@gmail.com>
2024-10-22 08:28:46 -0400

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def signal2():
-    print("signal 2 that views is running");
-    
+
 """    
 def my_view(request):
     return render(request, 'my_template.html', {'message': 'Hello from my view!'})
-"""
+
 
 
 def my_view(request):
     return render(request, {'message': 'Hello from my view!'});
     
- 
+
 def index(request):
     print('---- Index triggered ------ ');
     return HttpResponse("Hello, world. You're at the polls index.");
-
+"""
 def entry(request):
     #print('---- Main triggered ------ ');
-    return HttpResponse("You're at the main-entry.");
+    return HttpResponse("main reporting zeist++");
 
 
 
