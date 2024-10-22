d4b08c547ccc260562bc62c114e051fd99d88fba
mm3023 <mikemoore227@gmail.com>
2024-10-22 08:39:35 -0400

    Update views.py

--- mysite/TS1Proj/views.py
+++ mysite/TS1Proj/views.py
@@ -1,38 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def zeist_initiate():
+    print("zeist is online");
+    return 0;
+zeist_initiate()
 
-
-    
-"""    
-def my_view(request):
-    return render(request, 'my_template.html', {'message': 'Hello from my view!'})
-
-
-
-def my_view(request):
-    return render(request, {'message': 'Hello from my view!'});
-    
- 
-def index(request):
-    print('---- Index triggered ------ ');
-    return HttpResponse("Hello, world. You're at the polls index.");
-
-def entry(request):
-    #print('---- Main triggered ------ ');
-    return HttpResponse("You're at the main-entry.");
-"""
-    
-"""
-def index(request):
-    print('---- Index triggered ------ ');
-    return HttpResponse("Hello, world. You're at the TS1 index.");
-"""
-"""
-def index(request):
-    title={'title':'VerbalOctopus'};
-    return render(request,'template.html',title,{'message': 'Hello from my TS1Proj!'});
-"""
 def index(request):
         context={'message': 'Hello from my TS1Proj!','title':'Verbal Octopus'}
         return render(request,'template.html',context);
