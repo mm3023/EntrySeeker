fd510693e23df6c031fdb0bc07621d7635ecf143
mm3023 <mikemoore227@gmail.com>
2024-10-22 23:24:06 -0400

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -10,10 +10,10 @@ zeist_initiate();
 #response = requests.get('https://api.github.com/user', auth=('user', 'pass'))
 #https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25
 response1 = requests.get('https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25').json();
-
-response_1=pandas.read_json(response1);
-print(response_1);
-
+print(response1);
+#response_1=pandas.read_json(response1);
+#print(response_1);
+"""
 print("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
 print("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
 
@@ -22,7 +22,7 @@ print(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 response2 = requests.get('https://date.nager.at/api/v3/PublicHolidays/2024/US').json();
 response_2=pandas.read_json(response2);
 print(response_2);
-
+"""
 
 
 
