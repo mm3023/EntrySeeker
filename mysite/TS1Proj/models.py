577a6f90eca86ae90d6e649828e2df2ee43c42dc
mm3023 <mikemoore227@gmail.com>
2024-10-22 22:56:11 -0400

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -1,1 +1,27 @@
+import requests;
+import json
+import pandas
+
+def zeist_initiate():
+    print("zeist is online");
+    return 0;
+zeist_initiate();
+
+#response = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+#https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25
+response1 = requests.get('https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25');
+response1=response1.read_json();
+print(response1);
+
+print("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+print("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+
+#nager api alter year/country code;
+#https://date.nager.at/api/v3/PublicHolidays/2024/US
+response2 = requests.get('https://date.nager.at/api/v3/PublicHolidays/2024/US');
+response2=response2.read_json();
+print(response2);
+
+
+
 
