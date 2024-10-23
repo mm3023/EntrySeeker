8f29a518dabeb5e96087acb37a153fb966af2c58
mm3023 <mikemoore227@gmail.com>
2024-10-23 10:54:05 -0400

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -42,7 +42,7 @@ def date_nager_at():
      dateString=str(datetime.now())[0:4];
      print(dateString);
      #Original URL from docs https://date.nager.at/api/v3/PublicHolidays/2024/US;
-     URL=https://date.nager.at/api/v3/PublicHolidays/+dateString+/US;
+     URL=https://date.nager.at/api/v3/PublicHolidays/+dateString+/US
      print('URL');
      print(URL);
      #return JsonRequestResolver1(URL);
