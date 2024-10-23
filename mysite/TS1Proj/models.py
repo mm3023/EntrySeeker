ecf5afe43cf3a5ea3585aa3c067c4308eff7545f
mm3023 <mikemoore227@gmail.com>
2024-10-22 23:35:15 -0400

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -11,20 +11,22 @@ zeist_initiate();
 #https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25
 response1 = requests.get('https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25').json();
 print(response1);
-pandas.read_json(response1);
+print(response1)[1];
+#pandas.read_json(response1);
 
 #response_1=pandas.read_json(response1);
 #print(response_1);
-"""
+
 print("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
 print("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
 
 #nager api alter year/country code;
 #https://date.nager.at/api/v3/PublicHolidays/2024/US
 response2 = requests.get('https://date.nager.at/api/v3/PublicHolidays/2024/US').json();
+response_2=pandas.read_json(response2[1]);
 response_2=pandas.read_json(response2);
 print(response_2);
-"""
+
 
 
 
