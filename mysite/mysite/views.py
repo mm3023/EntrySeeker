ead386b5ddefcd787b00d1e3aa09ee0da8f7c801
mm3023 <mikemoore227@gmail.com>
2024-10-28 05:28:09 -0400

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -62,7 +62,7 @@ def upload(request):
     print(request.FILES);
     
     print(os.getcwd());
-    os.chdir(/var/www/);
+    os.chdir("/var/www/");
     print(os.listdir());
     #print(request.query_params);
     #print(request.data);
