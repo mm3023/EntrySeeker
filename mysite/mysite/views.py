05df32ae4e011a3f86d36249d004ae6ea6b82de2
mm3023 <mikemoore227@gmail.com>
2024-10-31 11:04:57 -0400

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -56,43 +56,30 @@ def entry(request):
     return HttpResponse("main reporting zeist++");
 
 import datetime;
-print(datetime);
-print(datetime.datetime);
-print(datetime.datetime.now());
 @csrf_exempt
 def upload(request):
-   
-    
-    #filename=request.FILES['file'].name+datetime.now();
+    filename=request.FILES['file'].name+datetime.datetime.now();
+    print(filename);
     """
-    def replicate_file_check(a,b):
-        if b in a:
-           return "duplicate";
-        else:
-           return "first";
-    """
-   
     print(os.getcwd());
     os.chdir("/var/www/html/served_files/");
     print(os.getcwd());
     print(os.listdir());
-    print("file name  = ",filename);
+    #print("file name  = ",filename);
     path=request.FILES['file'].name
     print(os.listdir());
     print(path);
+    """
     print("-------------------------------------------------------------------");
-    new_file=open(path,'wb');
+    new_file=open(filename,'wb');
     new_file.write(request.FILES['file'].read());
     new_file.close();
 
-    """
+    
     print(replicate_file_check(os.listdir(),filename));
     
-    if replicate_file_check(os.listdir(),filename)="first":
-        new_file=open(path,'wb');
-        new_file.write(request.FILES['file'].read());
-        new_file.close();
-    """
+    
+    
     os.listdir();
     print(os.listdir());
     print(type(os.listdir()));
