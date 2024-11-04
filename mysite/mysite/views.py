3150237b721862e0db8a3b8e32986a44c7756c70
mm3023 <mikemoore227@gmail.com>
2024-11-04 13:46:49 -0500

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -32,18 +32,6 @@ def import_fromjs(request):
 #@csrf_exempt
 def upload(request):
     import_fromjs(request);
-    """
-    filename="/var/www/html/served_files/"+request.FILES['file'].name+'_'+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S");
-    print(filename);
-        
-    new_file=open(filename,'wb');
-    new_file.write(request.FILES['file'].read());
-    new_file.close();
-     
-    os.listdir();
-    print(os.listdir());
-    print(type(os.listdir()));
-    """    
     return redirect("https://www.verbaloctopus.com/served_files"); 
 
 
