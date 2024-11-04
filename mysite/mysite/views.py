9e2596e10f373dfc91a718d80392096128c6eb06
mm3023 <mikemoore227@gmail.com>
2024-11-04 13:53:00 -0500

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect
 import os
-import handlerFunctions
+from.import handlerFunctions
 
 
 
