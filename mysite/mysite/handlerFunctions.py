d0476019a113c7ce3cc33e81e3b5f5666fa3fef4
mm3023 <mikemoore227@gmail.com>
2024-11-04 13:47:10 -0500

    Update handlerFunctions.py

--- mysite/mysite/handlerFunctions.py
+++ mysite/mysite/handlerFunctions.py
@@ -1,1 +1,14 @@
 
+
+
+def import_fromjs(request):
+    filename="/var/www/html/served_files/"+request.FILES['file'].name+'_'+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S");
+    print(filename);
+        
+    new_file=open(filename,'wb');
+    new_file.write(request.FILES['file'].read());
+    new_file.close();
+     
+    os.listdir();
+    print(os.listdir());
+    print(type(os.listdir()));
