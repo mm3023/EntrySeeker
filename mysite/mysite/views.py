f8f26b7d85919a1e03bbf9e9e7d23e2c6352d3b3
mm3023 <mikemoore227@gmail.com>
2024-11-08 10:55:24 -0500

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -28,58 +28,9 @@ def clear_served_files(request):
     for files in os.listdir() :
         os.remove(files);
     print("os.listdir() -- ",os.listdir());    
-    #print("----permissions R ?--",os.access(os.getcwd(),os.R_OK));
-    #print("find True ",str(os.access(os.getcwd(),os.R_OK)).find("True"));
-    """
-    File_Path-=os.getcwd();
-    if str(os.access(File_Path-,os.W_OK)).find("True"):
-       print("Permission to Write");
-    else: 
-        print("Permission not given chmod");
-    if     
-    print("check 1");
-    """
     
     
-    
-    
-    """
-    print("os.getcwd()--",os.getcwd());
-    os.chdir("/var/www/html/served_files")
-    print("os.getcwd()--",os.getcwd());
-    print("os.listdir() -- ",os.listdir());
-    
-
-
-
-
-
-    
-    print("----permissions R ?--",os.access(os.getcwd(),os.R_OK));
-    print("os.listdir()[0]----",os.listdir()[0]);
-    print("os.listdir()[1]----",os.listdir()[1]);
-    print("os.listdir()[2]----",os.listdir()[2]);
-    print("check point 1")
-    os.path.isfile(os.listdir()[0]).find('True');
-    os.path.isfile(os.listdir()[0])
-    print("check point 2")
-    print(os.path.isfile(os.listdir()[0]));
-    print(type(os.path.isfile(os.listdir()[0])));
-    print("check point 3")
-    print(os.path.isfile(os.listdir()[0]));
-    print("check point 4")
-    os.listdir()[0].find('/')
-    print("check point 5")
-    str(os.path.isfile(os.listdir()[0]))
-    print(str(os.path.isfile(os.listdir()[0])))
-    print(type(str(os.path.isfile(os.listdir()[0]))))
-    print("check point 6")
-    str(os.path.isfile(os.listdir()[0])).find('/');
-    print(str(os.path.isfile(os.listdir()[0])).find('/'))
-    
-    print(os.path.isfile(os.listdir()[0]).find('/'));
-    print("check point 7")
-    """
+  
     return redirect('clear_served_files()');
     #return 0;
     
