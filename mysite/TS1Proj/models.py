0bed54d86ab690c562998746ca0b463ec67b61b9
mm3023 <mikemoore227@gmail.com>
2024-11-13 14:51:34 -0500

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from django import forms
 
 #google api imports
-from __future__ import print_function
+#from __future__ import print_function
 from googleapiclient.discovery import build
 #from google.oauth2.credentials import Credentials   
 #from datetime import datetime, timedelta   
