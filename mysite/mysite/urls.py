bc31d29e4e4bac720a979d8896f79ddef11700d8
mm3023 <mikemoore227@gmail.com>
2024-11-15 14:42:17 -0500

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -32,6 +32,7 @@ print("from urls");
 print("api test");
 print(requests.get("https://www.googleapis.com/calendar/v3"));
 print(requests.get("https://www.googleapis.com/calendar/v3/users/me/calendarList"));
+print("os.getcwd() - ",os.getcwd());
 googcal.main();
 
 
