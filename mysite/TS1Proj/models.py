7b4b5e78ab50694a2526168f0041910e5110b1ec
mm3023 <mikemoore227@gmail.com>
2024-11-15 09:00:35 -0500

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -7,6 +7,7 @@ from google.oauth2.credentials import Credentials
 #from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 from datetime import datetime, timedelta 
 import requests;
