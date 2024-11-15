20c00b5e069ef1359b220711f47b45291b06200b
mm3023 <mikemoore227@gmail.com>
2024-11-15 08:34:08 -0500

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -2,44 +2,15 @@ from __future__ import print_function
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 
-
-
-
-
 from datetime import datetime, timedelta 
-
-
 import requests;
 import json
 import pandas
-#from datetime import datetime
 from django import forms
-
-#google api imports
-#from __future__ import print_function
-#from googleapiclient.discovery import build
-#from google.oauth2.credentials import Credentials   
-#from datetime import datetime, timedelta   
 
 calendar_id="arsaccess2022@gmail.com";
 
 
-"""
-from django.db import models
-
-class MyModel(models.Model):
-    file = models.FileField(upload_to='uploads/')
-class MyModel(models.Model):
-    file = models.FileField(upload_to='uploads/')
-
-
-
-class MyForm(forms.ModelForm):
-    class Meta:
-        model = MyModel
-        fields = ['file']
-
-"""
 
 
 
@@ -47,13 +18,8 @@ class MyForm(forms.ModelForm):
 
 
 
-"""
 
-def zeist_initiate():
-    print("zeist is online");
-    return 0;
-#zeist_initiate();
-"""
+
 def JsonRequestResolver1(x):
         #test_for_string=type(x);
         if type(x)!=type('x'):
@@ -65,11 +31,7 @@ def JsonRequestResolver1(x):
             resFrame=pandas.DataFrame(response);
             #reFrame=[item.split(",") for item in resFrame];
         return resFrame;
-
-
-
-
-
+#generic calender holidays
 def open_holiday_DataFrame():
     dateString=str(datetime.now())[0:10];
     #original URL from documentation openholidaysapi 'https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25';
@@ -80,14 +42,11 @@ def open_holiday_DataFrame():
     return JsonRequestResolver1('https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25');
 
 
-
+#deprecate? troubleshoot? usually empty
 def nagerAPI():
-     
-     dateString=str(datetime.now())[0:4];
-     
-     
-     print(dateString);
-     print(type(dateString));
+    dateString=str(datetime.now())[0:4];
+    print(dateString);
+    print(type(dateString));
      
      #Original URL from docs 'https://date.nager.at/api/v3/PublicHolidays/2024/US'
      URL='https://date.nager.at/api/v3/PublicHolidays/'+dateString+'/US'
@@ -96,25 +55,18 @@ def nagerAPI():
      return JsonRequestResolver1(URL);
      
     
-#open_holiday_DataFrame();
-     
-
-#open_holiday_set=JsonRequestResolver1('https://openholidaysapi.org/PublicHolidaysByDate?languageIsoCode=DE&date=2023-12-25');
 
 
 
 
-#print("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
-#print("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+
 
 #nager api alter year/country code;
 #https://date.nager.at/api/v3/PublicHolidays/2024/US
 print('open_holiday_DataFrame()');
 print(open_holiday_DataFrame());
 
-#date_nager_at=JsonRequestResolver1('https://date.nager.at/api/v3/PublicHolidays/2024/US');
-#print('nagerAPI()');
-#print(nagerAPI());
+
 
 
 
