c230cea5cd1e558c9b88446add34fce6d3392c1d
mm3023 <mikemoore227@gmail.com>
2024-11-16 16:53:27 -0500

    Update googcal.py

--- mysite/mysite/googcal.py
+++ mysite/mysite/googcal.py
@@ -36,6 +36,7 @@ def main():
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
   # time.
+  """
   if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
   # If there are no (valid) credentials available, let the user log in.
@@ -50,7 +51,7 @@ def main():
     # Save the credentials for the next run
     with open("token.json", "w") as token:
       token.write(creds.to_json())
-
+  """
   try:
     service = build("calendar", "v3", credentials=creds)
 
