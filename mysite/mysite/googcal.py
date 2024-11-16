090c932f276cdd83df5c637707d1ad8f25527dd0
mm3023 <mikemoore227@gmail.com>
2024-11-16 16:51:01 -0500

    Update googcal.py

--- mysite/mysite/googcal.py
+++ mysite/mysite/googcal.py
@@ -30,7 +30,9 @@ def main():
   """Shows basic usage of the Google Calendar API.
   Prints the start and name of the next 10 events on the user's calendar.
   """
-  creds = None
+  #creds = None
+  
+  creds = service_account.Credentials.from_service_account_file('credentials.json', scopes=SCOPES)
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
   # time.
