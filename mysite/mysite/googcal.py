be533cd3eea1c57146953f07c6d160227c3c198f
mm3023 <mikemoore227@gmail.com>
2024-11-16 16:12:09 -0500

    Update googcal.py

--- mysite/mysite/googcal.py
+++ mysite/mysite/googcal.py
@@ -10,7 +10,7 @@ from googleapiclient.errors import HttpError
 from google.oauth2 import service_account
 
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
-#creds = service_account.Credentials.from_service_account_file('credentials.json', scopes=SCOPES)
+creds = service_account.Credentials.from_service_account_file('credentials.json', scopes=SCOPES)
 #creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 #creds = service_account.Credentials.from_json_keyfile_name(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 #creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
