1c8a9951edce5eac8a36a9943ec1c0ab01aa8b5d
mm3023 <mikemoore227@gmail.com>
2024-11-18 09:07:13 -0500

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -65,15 +65,17 @@ def holidays():
     #h1=pandas.DataFrame(open('holiday.csv', 'r').read()); 
     #h2=pandas.DataFrame(open('new_holidays.csv', 'r').read()) 
        
-    holiday_doc1=open('holiday.csv', 'r');  
-    holiday_doc2=open('new_holidays.csv', 'r');  
-    h1=pandas.DataFrame(holiday_doc1.read());
-    h2=pandas.DataFrame(holiday_doc2.read())   
-    holiday_doc2.read()   
-    print(holiday_doc1.read()); 
-    print(holiday_doc2.read()); 
-    print(h2); 
-    print(h2);    
+    #holiday_doc1=open('holiday.csv', 'r');  
+    #holiday_doc2=open('new_holidays.csv', 'r'); 
+    holiday_doc1=pandas.read_csv('holiday.csv');   
+    holiday_doc2pandas.read_csv('holiday.csv');      
+    #h1=pandas.DataFrame(holiday_doc1.read());
+    #h2=pandas.DataFrame(holiday_doc2.read())   
+    #holiday_doc2.read()   
+    print(holiday_doc1); 
+    print(holiday_doc2); 
+    #print(h2); 
+    #print(h2);    
     print('end holidays')
    
     return 0;
