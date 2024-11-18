9613bede6846e621b511ca7f31ac15e988905194
mm3023 <mikemoore227@gmail.com>
2024-11-18 08:37:15 -0500

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -59,7 +59,7 @@ def holidays():
     #loop_holiday_api(current_year);
     loop_holiday_api(next_year);
        
-    holiday_doc=open('holiday.csv', RW);
+    holiday_doc=open('holiday.csv', 'rw');
     print('os.getcwd() - ',os.getcwd());   
     print('os.listdir() - ',os.listdir());
     print(holiday_doc);   
