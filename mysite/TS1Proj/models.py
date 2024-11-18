13ebb56db6343404dee3fc7e3dc7e7d1dcd87bb0
mm3023 <mikemoore227@gmail.com>
2024-11-18 12:44:50 -0500

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -65,7 +65,7 @@ def holidays():
     holiday_name=[];
     Month=[];
     Day=[];
-    #date=[];
+    date=[];
     data={'Holiday_Name':holiday_name,'Month':Month,'Day':Day}
     next_year=datetime.today() + timedelta(days=365);
     next_year=next_year.year
@@ -79,7 +79,7 @@ def holidays():
         while count < len(country_holidays('US', years=_year_).keys()):
               holiday_name.append(list(country_holidays('US', years=_year_).values())[count]);
               #print('holiday name = ',list(country_holidays('US', years=_year_).values())[count]);
-              #date.append(list(country_holidays('US', years=_year_).keys())[count]);
+              date.append(list(country_holidays('US', years=_year_).keys())[count]);
               #print('key ',list(country_holidays('US', years=_year_).keys())[count]) 
               #print('key type',type(list(country_holidays('US', years=_year_).keys())[count]))
               #'key type',list(country_holidays('US', years=_year_).keys())[count] 
