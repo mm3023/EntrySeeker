0feb1c9f3cf500e83a78f56af937df322b1c72f3
mm3023 <mikemoore227@gmail.com>
2024-11-18 11:54:44 -0500

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -48,7 +48,7 @@ def holidays():
     #print(datetime.now().day - timedelta(days=10))   
     holiday_doc=pandas.read_csv('TS1Proj/geminiholidays.csv');   
     holiday_doc.loc[holiday_doc[Month]=datetime.now().month];
-    print(holiday_doc.loc[holiday_doc[Month]=datetime.now().month])   
+    print(holiday_doc.loc[holiday_doc[Month]=>datetime.now().month])   
   
 
 
