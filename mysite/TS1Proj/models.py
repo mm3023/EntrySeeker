a45665b9107281fb17ad841da999d5687aecda9e
mm3023 <mikemoore227@gmail.com>
2024-11-18 08:24:06 -0500

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -29,28 +29,27 @@ import holidays;
 
 
 def holidays():
-    holiday_name=[];
-    year=[];
-    date=[];
-    #def populate_arrays():
-    
+       
     print("inside holidays-----")
+    
     #datetime.today()+timedelta(days=1);
+    """
     next_year=datetime.today() + timedelta(days=365);
     next_year=next_year.year
     current_year=datetime.today().year;
+    """
     #next_year_str=f'{next_year.year}';
     #print("current_year ",current_year,type(current_year));
     #print("next_year ",next_year,type(next_year));
-    country_holidays('US', years=current_year);
-    country_holidays('US', years=next_year);
+    #country_holidays('US', years=current_year);
+    #country_holidays('US', years=next_year);
     #print("country holidays  ",type(country_holidays('US', years=current_year)) ,country_holidays('US', years=current_year))
     #print("country holidays  subsribe ",type(country_holidays('US', years=current_year)) ,country_holidays('US', years=current_year)[5])
-    count=0;
-    country_holidays('US', years=current_year).items();
-    
-    the_holidays=country_holidays('US', years=current_year);
-    the_holidays.items();
+    #count=0;
+    #country_holidays('US', years=current_year).items();
+    #the_holidays=country_holidays('US', years=current_year);
+    #the_holidays.items();
+    """
     print("country_holidays('US', years=current_year).items() ---------------------- ",country_holidays('US', years=current_year).items())
     print('the_holidays.items() ---------------------',the_holidays.items())
     print('the_holidays.keys() ---------------------',the_holidays.keys())
@@ -58,7 +57,19 @@ def holidays():
     print(' the_holidays.values() ---------------------',the_holidays.values())
     print('list the_holidays.values()[0] ---------------------',list(the_holidays.values())[0])
     print('the_holidays ---------------------',the_holidays)
-
+    """
+    
+    
+    
+    holiday_name=[];
+    year=[];
+    date=[];
+    
+    next_year=datetime.today() + timedelta(days=365);
+    next_year=next_year.year
+    current_year=datetime.today().year;
+    the_holidays=country_holidays('US', years=current_year);
+    the_holidays.items();
     def loop_holiday_api(_year_):
         count=0;
         while count < len(country_holidays('US', years=current_year).keys()):
@@ -67,18 +78,10 @@ def holidays():
               date.append(list(country_holidays('US', years=_year_).keys())[count]);
               print('date = ',list(country_holidays('US', years=_year_).keys())[count]);
               year.append(current_year);
-        count=count+1;
-    loop_holiday_api(current_year)
-    """
-    count=0;
-    
-    while count < len(country_holidays('US', years=current_year).keys()):
-        holiday_name.append(country_holidays('US', years=current_year).keys()[count]);
-        print('holiday name = ',country_holidays('US', years=current_year).keys()[count]);
-        date.append(country_holidays('US', years=current_year).values()[count]);
-        print('date = ',country_holidays('US', years=current_year).values()[count]);
-        year.append(current_year)    
-    """    
+              count=count+1;
+    loop_holiday_api(current_year);
+    loop_holiday_api(next_year);
+   
    
     print('end holidays')
    
