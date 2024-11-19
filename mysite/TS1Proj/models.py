703eae9874c10698cf248df60439a0013f59d514
mm3023 <mikemoore227@gmail.com>
2024-11-19 15:31:29 -0500

    Update models.py

--- mysite/TS1Proj/models.py
+++ mysite/TS1Proj/models.py
@@ -119,7 +119,7 @@ def Holiday_selector():
     count=0;
     sorting_ints=[];
     print('len of arr ',len(upcommingHolidays['date'][count]));
-    while len(upcommingHolidays['date'][count])+1 > count:
+    while count < len(upcommingHolidays['date'][count]):
         print('count - ',count,"-",int(upcommingHolidays['date'][count][3:]));
         print(sorting_ints);
         print('len sorting units ',len(sorting_ints));
